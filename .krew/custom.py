3ac146fd7e4109b8a1dc7824ea617c65dfcac5d5
jpts <jpts@users.noreply.github.com>
2022-08-29 23:28:56 +0100

    ci: test krew template generation

---
+++ .krew/custom.py
@@ -0,0 +1,25 @@
+import os
+
+def alter_context(context):
+    """ Modify the context and return it """
+    context['tag'] = os.environ['GITHUB_REF_NAME']
+    return context
+
+
+
+def extra_filters():
+    """ Declare some custom filters.
+        Returns: dict(name = function)
+    """
+    return dict(
+        sha256=lambda f: getChecksum(f),
+    )
+
+f = open("dist/checksums.txt", "r")
+sums = f.readlines()
+f.close()
+
+def getChecksum(filename):
+    for line in sums:
+        if line.find(filename.strip()) >= 0:
+            return line.split(' ')[0]
